88da69bfe86d55b472f679fa3bbc14ee737744a2
Paweł Choniawko <pawel.choniawko@gmail.com>
2018-12-08 19:46:34 +0100

    hooks refactoring

--- src/Chat/ChatForm/index.js
+++ src/Chat/ChatForm/index.js
@@ -1,41 +1,40 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { compose } from "react-apollo";
 import { graphqlMutation } from "aws-appsync-react";
 import { AddMessage } from "./mutations";
 
-class ChatFormComponent extends Component {
-    state = { message: "" };
-    handleChange = e =>
-        this.setState({ message: e.target.value });
-    handleSubmit = e => {
+// TODO: authentication
+// TODO: conversation service
+const defaultConversation =
+    "4a37fe43-c74a-4fa7-9b6c-376b92f00db5";
+const authorId = "5f3c7388-e7de-4aca-be7d-b10db835865b";
+
+const ChatFormComponent = ({ createMessage }) => {
+    const [message, setMessage] = useState("");
+    const handleChange = e => setMessage(e.target.value);
+    const handleSubmit = e => {
         e.preventDefault();
-        console.log(this.props);
-        this.props.createMessage({
-            authorId:
-                "5f3c7388-e7de-4aca-be7d-b10db835865b",
-            conversationId:
-                "4a37fe43-c74a-4fa7-9b6c-376b92f00db5",
-            content: this.state.message
+        createMessage({
+            authorId,
+            conversationId: defaultConversation,
+            content: message
         });
-        this.setState({ message: "" });
+        setMessage("");
     };
-    render() {
-        const { handleSubmit, handleChange } = this;
-        const { message } = this.state;
-        return (
-            <div className="ChatForm">
-                <form onSubmit={handleSubmit}>
-                    <input
-                        onChange={handleChange}
-                        value={message}
-                        type="text"
-                    />
-                    <button>Send</button>
-                </form>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="ChatForm">
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={handleChange}
+                    value={message}
+                    type="text"
+                />
+                <button>Send</button>
+            </form>
+        </div>
+    );
+};
+
 export const ChatForm = compose(
     graphqlMutation(AddMessage)
 )(ChatFormComponent);
