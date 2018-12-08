94a9f5b2f71d313836ddd059ede2c93e18dadb6a
Paweł Choniawko <pawel.choniawko@gmail.com>
2018-12-08 19:48:10 +0100

    clean mutations

--- src/Chat/ChatForm/mutations.js
+++ src/Chat/ChatForm/mutations.js
@@ -1,9 +1,5 @@
 import gql from "graphql-tag";
 
-// authorId: "5f3c7388-e7de-4aca-be7d-b10db835865b"
-// conversationId: "4a37fe43-c74a-4fa7-9b6c-376b92f00db5"
-// content: "test message"
-
 export const AddMessage = gql`
     mutation(
         $authorId: ID!
